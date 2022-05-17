9868fe1b14adb60e480f6b9add99eb7bf550fe76
PauloAlves8039 <pj38alves@gmail.com>
2022-05-17 08:45:49 -0300

    Organizando a estrutura inicial do projeto

---
+++ project-school/src/components/Alunos.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1>
+    <input type="text" placeholder="Nome do Aluno" v-model="nome">
+    <table border="1px">
+      <thead>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Paulo</td>
+          <td>
+            <button class="btn" @click="remover()">Remover</button>
+          </td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Isadora</td>
+          <td>
+            <button class="btn" @click="remover()">Remover</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Aluno',
+      nome: 'Alunos',
+    }
+  },
+  props: {},
+};
+</script>
+
+<style scoped>
+</style>
