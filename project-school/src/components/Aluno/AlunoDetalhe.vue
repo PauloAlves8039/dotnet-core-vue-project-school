3261bf6de92e341aa4813363cd07f697dfa00c00
PauloAlves8039 <pj38alves@gmail.com>
2022-05-31 08:58:56 -0300

    Adicionando o componente Aluno Detalhe

---
+++ project-school/src/components/Aluno/AlunoDetalhe.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <titulo :texto="`Aluno: ${aluno.nome}`" />
+    <table>
+      <tbody>
+        <tr>
+          <td class="colPequeno">Matrícula:</td>
+          <td>{{ aluno.id }}</td>
+        </tr>
+        <tr>
+          <td>Nome:</td>
+          <td>{{ aluno.nome }}</td>
+        </tr>
+        <tr>
+          <td>Sobrenome:</td>
+          <td>{{ aluno.sobrenome }}</td>
+        </tr>
+        <tr>
+          <td>Data Nascimento</td>
+          <td>{{ aluno.dataNasc }}</td>
+        </tr>
+        <tr>
+          <td>Professor:</td>
+          <td>{{ aluno.professor.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from "../_share/Titulo.vue";
+
+export default {
+  components: {
+    Titulo,
+  },
+  data() {
+    return {
+      aluno: {},
+      urlAluno: "http://localhost:3000/alunos",
+      id: this.$route.params.id,
+    };
+  },
+  created() {
+    this.$http
+      .get(`${this.urlAluno}/${this.id}`)
+      .then((res) => res.json())
+      .then((aluno) => (this.aluno = aluno));
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
--- project-school/src/router.js
+++ project-school/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Alunos from "./components/Aluno/Alunos";
+import AlunoDetalhe from "./components/Aluno/AlunoDetalhe";
 import Professor from "./components/Professor/Professor";
 import Sobre from "./components/Sobre/Sobre";
 
@@ -22,6 +23,11 @@ export default new Router({
       path: "/todosalunos",
       nome: "Alunos",
       component: Alunos,
+    },
+    {
+      path: "/alunoDetalhe/:id",
+      nome: "AlunoDetalhe",
+      component: AlunoDetalhe,
     },
     {
       path: "/sobre",
